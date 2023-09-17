f9fec9b779a2f7b499171da8f0c8f8080cf8d02e
AtomThum <atom.olee@gmail.com>
2023-09-17 16:11:14 +0700

    reformatted code to camel case

--- ellipserasterization.jl
+++ ellipserasterization.jl
@@ -7,14 +7,14 @@ function rasterellipse(a, b)
     x = 0
     y = b
 
-    points = Tuple{Int, Int}[] # Initialize a tuple to store the points in the ellipse
-    a_sqr = a*a # Precalc for saving computation time
-    b_sqr = b*b
-    a_sqr2 = 2*a_sqr 
-    b_sqr2 = 2*b_sqr
-    p1 = b_sqr - b*a_sqr + 1/4*a_sqr # Given by the properties of the ellipse, p of the first section
+    points = Tuple{Int,Int}[] # Initialize a tuple to store the points in the ellipse
+    aSqr = a * a # Precalc for saving computation time
+    bSqr = b * b
+    aSqr2 = 2 * aSqr
+    bSqr2 = 2 * bSqr
+    p1 = bSqr - b * aSqr + 1 / 4 * aSqr # Given by the properties of the ellipse, p of the first section
     dx = 0 # Initialize the step in the x direction
-    dy = a_sqr2 * b
+    dy = aSqr2 * b
 
     # Lower region mirror
     while dx < dy
@@ -22,31 +22,31 @@ function rasterellipse(a, b)
             (x, y), (-x, y), (x, -y), (-x, -y)
         ])
         x += 1
-        dx += b_sqr2
+        dx += bSqr2
         if p1 < 0
-            p1 += dx + b_sqr
+            p1 += dx + bSqr
         else
             y -= 1
-            dy -= a_sqr2
-            p1 += dx + b_sqr - dy
+            dy -= aSqr2
+            p1 += dx + bSqr - dy
         end
     end
 
     # Upper region mirror
-    p2 = b_sqr*(x + 1/2)^2 + a_sqr*(y - 1)^2 - a_sqr*b_sqr # p of the upper section
+    p2 = bSqr * (x + 1 / 2)^2 + aSqr * (y - 1)^2 - aSqr * bSqr # p of the upper section
     while y >= 0
         append!(points, [
             (x, y), (-x, y), (x, -y), (-x, -y)
         ])
         y -= 1
-        dy -= a_sqr2
+        dy -= aSqr2
 
         if p2 > 0
-            p2 += a_sqr - dy
+            p2 += aSqr - dy
         else
             x += 1
-            dx += b_sqr2
-            p2 += a_sqr - dy + dx
+            dx += bSqr2
+            p2 += aSqr - dy + dx
         end
     end
     return points
@@ -55,10 +55,10 @@ end
 # Wil be corrected later if possible
 # Rotation algorithm for any set of points. (Very Incomplete)
 function rotate(θ, setpoints)
-    newpoints = Tuple{Int, Int}[]
-    R = [cos(θ) -sin(θ); sin(θ) cos(θ)]
+    newpoints = Tuple{Int,Int}[]
+    rotationMatrix = [cos(θ) -sin(θ); sin(θ) cos(θ)]
     for i in setpoints
-        a = R * collect(i)
+        a = rotationMatrix * collect(i)
         floorx = floor(a[1])
         floory = floor(a[2])
         # ceilx = ceil(a[1])
@@ -70,20 +70,20 @@ function rotate(θ, setpoints)
 end
 
 # Translate a set of matrices
-function translate(setpoints, xtransposed, ytransposed)
-    newpoints = Tuple{Int, Int}[]
+function translate(setpoints, xTransposed, yTransposed)
+    newpoints = Tuple{Int,Int}[]
     for i in setpoints
         a = collect(i)
-        x = a[1] + xtransposed
-        y = a[2] + ytransposed
+        x = a[1] + xTransposed
+        y = a[2] + yTransposed
         push!(newpoints, (x, y))
     end
 end
 
 # Creating a column matrix for concatenation
-map(n, gridsize, x, y) = x + n + 1 - gridsize*(y - n)
+map(n, gridsize, x, y) = x + n + 1 - gridsize * (y - n)
 function vectorize(n, setpoints)
-    gridsize = 2*n + 1
+    gridsize = 2 * n + 1
     vect = zeros(gridsize^2, 1)
     for i in setpoints
         x, y = i[1], i[2]
@@ -101,17 +101,16 @@ end
 # display(vectorize(10, ellipse))
 
 n = 5 # 2n + 1 = gridsize
-N = (2*n + 1)^2
+N = (2 * n + 1)^2
 # Defining basis
-major_axis_basis = range(1, 5)
-
+majorAxisBasis = range(1, 5)
+minorAxisBasis = 3
 # Generating a matrix with the basis
-minor_axis_basis = 3
 basisMatrix = Array{Float64}(undef, N)
-for i in major_axis_basis
+for i in majorAxisBasis
     global n
     global basisMatrix
-    tempEllipse = rasterellipse(i, minor_axis_basis)
+    tempEllipse = rasterellipse(i, minorAxisBasis)
     tempVect = vectorize(n, tempEllipse)
     basisMatrix = hcat(basisMatrix, tempVect)
 end
